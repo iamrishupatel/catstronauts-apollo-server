bfe7e47875231acd83e6822129176bdf7814b011
Rishu <rishupatel04@gmail.com>
2022-05-11 16:02:22 +0530

    add real data instead of mock data
    This starts sending the real data to a client instead of a mock data
    - Lift-off II complete

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,13 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema.js");
-
-const mocks = {
-  Query: () => ({
-    getTracks: [...new Array(6)],
-  }),
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
 
 const server = new ApolloServer({
   typeDefs,
   csrfPrevention: true,
-  mocks,
+  resolvers,
+  dataSources: () => ({ trackAPI : new TrackAPI() }),
 });
 
 server.listen().then(() => {
